07ac687c85cfea46633d6c5b8c472752344f0990
LunarIGuess <tony567020@gmail.com>
2025-05-30 00:05:39 -0700

    Basic controls
    added controller support and wasd

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -3,7 +3,16 @@ using System;
 
 public partial class PlayerController : Node
 {
+    [Export]
+    public float speed = 20.0f;
+
+    public float shootingCD = 0.5f;
+
+    Vector3 PlayerLimit = new Vector3(0, 0, 0);
+
     Node3D player;
+
+
     public override void _Ready()
     {
         player = this.GetParent<Node3D>();
@@ -11,6 +20,24 @@ public partial class PlayerController : Node
 
     public override void _Process(double delta)
     {
-        base._Process(delta);
+        input(delta);
+    }
+    // input handler
+    private void input(double delta)
+    {
+        float Horizontal = Input.GetAxis("Left", "Right");
+        float Vertical = Input.GetAxis("Up", "Down");
+
+        if (player != null)
+        {
+            // Moving controls
+            player.Position = player.Position += new Vector3(Horizontal * speed, 0, Vertical * speed).Normalized();
+
+            // holding down shoot button
+            if (Input.IsActionPressed("Shoot"))
+            {
+                GD.Print("shooting");
+            }
+        }
     }
 }
